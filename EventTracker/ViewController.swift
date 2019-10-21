d78d203ccdc780cad0847f41d02ab766768da5ba
Noah Flaniken <nhflan@comcast.net>
2019-10-21 10:23:47 -0400

    Initial Commit

---
+++ EventTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventTracker
+//
+//  Created by Noah Flaniken on 10/21/19.
+//  Copyright © 2019 Noah Flaniken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
