3bf14e7db5912b8aae6b8d0abe6d5f1c086cb4c1
Noah Flaniken <nhflan@comcast.net>
2019-10-23 14:28:18 -0400

    homeview

---
+++ EventTracker/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  EventTracker
+//
+//  Created by Noah Flaniken on 10/21/19.
+//  Copyright © 2019 Noah Flaniken. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
